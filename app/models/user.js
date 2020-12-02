0425b2d729e4f046ff3f12ed077b7f14f7425c37
Fernando Moreira <fernandoepm.pro@gmail.com>
2020-12-02 14:54:40 -0300

    Adding bcrypt to encrypt the passwords

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let userSchema = new mongoose.Schema({
     name: String,
@@ -6,6 +7,24 @@ let userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now }
+});
+
+// Middleware para transformar a password em um hash. Roda antes de salvar no BD (o next é para passar pro próximo middleware):
+userSchema.pre('save', function (next) {
+    // Verificar se o password mudou:
+    if (this.isNew || this.isModified('password')) {
+        // O número 10 é o número de caracteres que serão adicionados à senha para a criptografia da mesma
+        bcrypt.hash(this.password, 10,
+            (err, hashedPassword) => {
+                if (err)
+                    next(err);
+                else {
+                    this.password = hashedPassword;
+                    next();
+                }
+            }
+        )
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
