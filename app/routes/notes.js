7789411929e34bb69bcd077529695dccc2839815
Fernando Moreira <fernandoepm.pro@gmail.com>
2020-12-04 15:36:44 -0300

    Adding note removal and searching

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -15,6 +15,19 @@ router.post('/', withAuth, async (req, res) => {
     }
 })
 
+// Deve ser passado antes do get /:id
+router.get('/search', withAuth, async (req, res) => {
+    const { query } = req.query;
+    try {
+        let notes = await Note
+            .find({ author: req.user._id })
+            .find({ $text: { $search: query } });
+        res.json(notes);
+    } catch (error) {
+        res.json({ error: error }).status(500);
+    }
+});
+
 router.get('/:id', withAuth, async (req, res) => {
     try {
         const { id } = req.params; // Pega o id dos parâmetros da URL
@@ -54,6 +67,20 @@ router.put('/:id', withAuth, async (req, res) => {
 
     } catch (error) {
         res.status(500).json({ error: 'Problem to update a note' });
+    }
+})
+
+router.delete('/:id', withAuth, async (req, res) => {
+    const { id } = req.params;
+    try {
+        let note = await Note.findById(id);
+        if (isOwner(req.user, note)) {
+            await note.delete();
+            res.json({ message: 'Ok' }).status(204);
+        } else
+            res.status(403).json({ error: 'Permission denied' })
+    } catch (error) {
+        res.status(500).json({ error: 'Problem to delete a note' });
     }
 })
 
